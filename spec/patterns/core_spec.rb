9bc5027160197716dff045f4237222704defead0
Ryan D. O'Keeffe <rdokeeffe@rageguy.spscommerce.com>
2015-03-25 16:19:31 -0500

    updated s3 and elb field names to match AWS specified names, add matching for TCP ELB logs, remove coercion from optional fields

--- spec/patterns/core_spec.rb
+++ spec/patterns/core_spec.rb
@@ -14,49 +14,49 @@ describe LogStash::Patterns::Core do
 
     sample "79a5 mybucket [06/Feb/2014:00:00:38 +0000] 192.0.2.3 79a5 3E57427F3EXAMPLE REST.GET.VERSIONING - \"GET /mybucket?versioning HTTP/1.1\" 200 - 113 - 7 - \"-\" \"S3Console/0.4\" -" do
       insist { subject["tags"] }.nil?
-      insist { subject["owner"] } == "79a5"
+      insist { subject["bucket_owner"] } == "79a5"
       insist { subject["bucket"] } == "mybucket"
       insist { subject["timestamp"] } == "06/Feb/2014:00:00:38 +0000"
-      insist { subject["clientip"] } == "192.0.2.3"
+      insist { subject["remote_ip"] } == "192.0.2.3"
       insist { subject["requester"] } == "79a5"
       insist { subject["request_id"] } == "3E57427F3EXAMPLE"
       insist { subject["operation"] } == "REST.GET.VERSIONING"
       insist { subject["key"] } == '-'
-      insist { subject["verb"] } == "GET"
+      insist { subject["method"] } == "GET"
       insist { subject["request"] } == "/mybucket?versioning"
       insist { subject["httpversion"] } == "1.1"
-      insist { subject["response"] } == 200
+      insist { subject["http_status"] } == 200
       insist { subject["error_code"] }.nil?
-      insist { subject["bytes"] } == 113
+      insist { subject["bytes_sent"] } == "113"
       insist { subject["object_size"] }.nil?
-      insist { subject["request_time_ms"] } == 7
-      insist { subject["turnaround_time_ms"] }.nil?
+      insist { subject["total_time"] } == "7"
+      insist { subject["turnaround_time"] }.nil?
       insist { subject["referrer"] } == "\"-\""
-      insist { subject["agent"] } == "\"S3Console/0.4\""
+      insist { subject["user_agent"] } == "\"S3Console/0.4\""
       insist { subject["version_id"] }.nil?
     end
 
     sample "79a5 mybucket [12/May/2014:07:54:01 +0000] 10.0.1.2 - 7ACC4BE89EXAMPLE REST.GET.OBJECT foo/bar.html \"GET /foo/bar.html HTTP/1.1\" 304 - - 1718 10 - \"-\" \"Mozilla/5.0\" -" do
       insist { subject["tags"] }.nil?
-      insist { subject["owner"] } == "79a5"
+      insist { subject["bucket_owner"] } == "79a5"
       insist { subject["bucket"] } == "mybucket"
       insist { subject["timestamp"] } == "12/May/2014:07:54:01 +0000"
-      insist { subject["clientip"] } == "10.0.1.2"
+      insist { subject["remote_ip"] } == "10.0.1.2"
       insist { subject["requester"] } == "-"
       insist { subject["request_id"] } == "7ACC4BE89EXAMPLE"
       insist { subject["operation"] } == "REST.GET.OBJECT"
       insist { subject["key"] } == "foo/bar.html"
-      insist { subject["verb"] } == "GET"
+      insist { subject["method"] } == "GET"
       insist { subject["request"] } == "/foo/bar.html"
       insist { subject["httpversion"] } == "1.1"
-      insist { subject["response"] } == 304
+      insist { subject["http_status"] } == 304
       insist { subject["error_code"] }.nil?
-      insist { subject["bytes"] }.nil?
-      insist { subject["object_size"] } == 1718
-      insist { subject["request_time_ms"] } == 10
-      insist { subject["turnaround_time_ms"] }.nil?
+      insist { subject["bytes_sent"] }.nil?
+      insist { subject["object_size"] } == "1718"
+      insist { subject["total_time"] } == "10"
+      insist { subject["turnaround_time"] }.nil?
       insist { subject["referrer"] } == "\"-\""
-      insist { subject["agent"] } == "\"Mozilla/5.0\""
+      insist { subject["user_agent"] } == "\"Mozilla/5.0\""
       insist { subject["version_id"] }.nil?
     end
   end
@@ -81,16 +81,40 @@ describe LogStash::Patterns::Core do
       insist { subject["request_processing_time"] } == 0.000073
       insist { subject["backend_processing_time"] } == 0.001048
       insist { subject["response_processing_time"] } == 0.000057
-      insist { subject["response"] } == 200
-      insist { subject["backend_response"] } == 200
+      insist { subject["elb_status_code"] } == "200"
+      insist { subject["backend_status_code"] } == "200"
       insist { subject["received_bytes"] } == 0
-      insist { subject["bytes"] } == 29
-      insist { subject["verb"] } == "GET"
+      insist { subject["sent_bytes"] } == 29
+      insist { subject["method"] } == "GET"
       insist { subject["request"] } == "http://www.example.com:80/"
-      insist { subject["proto"] } == "http"
+      insist { subject["protocol"] } == "http"
       insist { subject["httpversion"] } == "1.1"
       insist { subject["urihost"] } == "www.example.com:80"
       insist { subject["path"] } == "/"
+      insist { subject["params"] }.nil?
+    end
+
+    sample "2015-03-25T19:43:47.565341Z my-test-tcp-loadbalancer 192.168.131.39:2817 10.0.0.1:80 0.000907 0.00001 0.000015 - - 25 0 \"- - - \"" do
+      insist { subject["tags"] }.nil?
+      insist { subject["timestamp"] } == "2015-03-25T19:43:47.565341Z"
+      insist { subject["elb"] } == "my-test-tcp-loadbalancer"
+      insist { subject["clientip"] } == "192.168.131.39"
+      insist { subject["clientport"] } == 2817
+      insist { subject["backendip"] } == "10.0.0.1"
+      insist { subject["backendport"] } == 80
+      insist { subject["request_processing_time"] } == 0.000907
+      insist { subject["backend_processing_time"] } == 0.00001
+      insist { subject["response_processing_time"] } == 0.000015
+      insist { subject["elb_status_code"] }.nil?
+      insist { subject["backend_status_code"] }.nil?
+      insist { subject["received_bytes"] } == 25
+      insist { subject["sent_bytes"] } == 0
+      insist { subject["method"] }.nil?
+      insist { subject["request"] }.nil?
+      insist { subject["protocol"] }.nil?
+      insist { subject["httpversion"] }.nil?
+      insist { subject["urihost"] }.nil?
+      insist { subject["path"] }.nil?
       insist { subject["params"] }.nil?
     end
   end
