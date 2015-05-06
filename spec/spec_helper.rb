18e72fb3cb01c75498252f81ce0dcd7f7ab8cab6
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-05-06 09:48:53 +0200

    Version 0.1.10 bump, including a fix for the logstash_home issue

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 require "logstash/devutils/rspec/spec_helper"
-require "logstash/filters/grok"
 require 'rspec/expectations'
+
+# running the grok code outside a logstash package means
+# LOGSTASH_HOME will not be defined, so let's set it here
+# before requiring the grok filter
+unless LogStash::Environment.const_defined?(:LOGSTASH_HOME)
+  LogStash::Environment::LOGSTASH_HOME = File.expand_path("../", __FILE__)
+end
+
+require "logstash/filters/grok"
 
 module GrokHelpers
   def grok_match(label, message)
