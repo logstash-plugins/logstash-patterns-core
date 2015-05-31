586933d7b144978f4cee1994fa724845baf4de04
Robert Habermann <mail@rhab.de>
2015-05-31 21:59:15 +0200

    add initial test for Cisco ASA firewall system log patterns
    Add initial test for Cisco ASA firewall system log patterns.

---
+++ spec/patterns/firewalls_spec.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+require "spec_helper"
+require "logstash/patterns/core"
+
+describe "FIREWALLS" do
+
+  let(:pattern)    { "CISCOFW104001" }
+
+  context "parsing a 104001 message" do
+
+    let(:value) { "(Secondary) Switching to ACTIVE - Service card in other unit has failed" }
+
+    subject     { grok_match(pattern, value) }
+
+    it "generates a message field" do
+      expect(subject["message"]).to include("(Secondary) Switching to ACTIVE - Service card in other unit has failed")
+    end
+  end
+
+end
