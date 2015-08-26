7549667d539fb205fb5ccf246f545c87b949bd64
Andreas Ntaflos <andreas.ntaflos@rise-world.com>
2015-08-26 02:28:35 +0200

    Support TIMESTAMP_ISO8601 in HAProxy patterns
    Rsyslog (and probably others) can be configured to use ISO8601
    timestamps, usually high-precision with microsecond resolution. The
    HAProxy patterns should take this possibility into account and not be
    arbitrarily limited to just SYSLOGTIMESTAMP.
    
    Fixes #80

--- spec/patterns/haproxy_spec.rb
+++ spec/patterns/haproxy_spec.rb
@@ -8,7 +8,23 @@ describe "HAPROXY" do
 
   context "Parsing HAPROXY log line from raw syslog line" do
 
-    let(:value) { 'Dec  9 13:01:26 localhost haproxy[28029]: 127.0.0.1:39759 [09/Dec/2013:12:59:46.633] loadbalancer default/instance8 0/51536/1/48082/99627 200 83285 - - ---- 87/87/87/1/0 0/67 {77.24.148.74} "GET /path/to/image HTTP/1.1"' } 
+    let(:value) { 'Dec  9 13:01:26 localhost haproxy[28029]: 127.0.0.1:39759 [09/Dec/2013:12:59:46.633] loadbalancer default/instance8 0/51536/1/48082/99627 200 83285 - - ---- 87/87/87/1/0 0/67 {77.24.148.74} "GET /path/to/image HTTP/1.1"' }
+    subject     { grok_match(haproxyhttp_pattern, value) }
+
+    it { should include("program" => "haproxy") }
+    it { should include("client_ip" => "127.0.0.1") }
+    it { should include("http_verb" => "GET") }
+    it { should include("server_name" => "instance8") }
+
+    it "generates a message field" do
+      expect(subject["message"]).to include("loadbalancer default/instance8")
+    end
+
+  end
+
+  context "Parsing HAPROXY log line from raw syslog line with ISO8601 timestamp" do
+
+    let(:value) { '2015-08-26T02:09:48+02:00 localhost haproxy[28029]: 127.0.0.1:39759 [09/Dec/2013:12:59:46.633] loadbalancer default/instance8 0/51536/1/48082/99627 200 83285 - - ---- 87/87/87/1/0 0/67 {77.24.148.74} "GET /path/to/image HTTP/1.1"' }
     subject     { grok_match(haproxyhttp_pattern, value) }
 
     it { should include("program" => "haproxy") }
@@ -26,7 +42,7 @@ describe "HAPROXY" do
 
   context "Parsing HAPROXY log line without syslog specific enteries.  This mimics an event coming from a syslog input." do
 
-    let(:value) { '127.0.0.1:39759 [09/Dec/2013:12:59:46.633] loadbalancer default/instance8 0/51536/1/48082/99627 200 83285 - - ---- 87/87/87/1/0 0/67 {77.24.148.74} "GET /path/to/image HTTP/1.1"' } 
+    let(:value) { '127.0.0.1:39759 [09/Dec/2013:12:59:46.633] loadbalancer default/instance8 0/51536/1/48082/99627 200 83285 - - ---- 87/87/87/1/0 0/67 {77.24.148.74} "GET /path/to/image HTTP/1.1"' }
     subject     { grok_match(haproxyhttpbase_pattern, value) }
 
     # Assume 'program' would be matched by the syslog input.
