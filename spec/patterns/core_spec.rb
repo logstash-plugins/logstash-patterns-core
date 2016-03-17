82b21c6806df638cfd7a755bd051702b87d4c187
Richard Gibson <richard.gibson@gmail.com>
2016-03-17 15:56:12 -0400

    Add URN pattern
    Fixes #136

--- spec/patterns/core_spec.rb
+++ spec/patterns/core_spec.rb
@@ -193,5 +193,92 @@ describe "IPV4" do
       expect(grok_match(pattern,value)).not_to pass
     end
   end
+end
 
+describe "URN" do
+
+  let(:pattern)       { "URN" }
+
+  # Valid URNs
+  # http://tools.ietf.org/html/rfc2141#section-2
+  let(:simple)        { "urn:example:foo" }
+  let(:unreserved)    { "urn:example:" +
+    [*'A'..'Z', *'a'..'z', *'0'..'9', "()+,-.::=@;$_!*'"].join() }
+  let(:reserved)      { "urn:example:/#?" }
+  let(:escaped_upper) { "urn:example:%25foo%2Fbar%3F%23" }
+  let(:escaped_lower) { "urn:example:%25foo%2fbar%3f%23" }
+  let(:only_escaped)  { "urn:example:%00" }
+  let(:long_nid)      { "urn:example-example-example-example-:foo" }
+
+  # Invalid URNs
+  let(:bad_prefix)     { "URN:example:foo" }
+  let(:empty_nid)      { "urn::foo" }
+  let(:leading_hyphen) { "urn:-example:foo" }
+  let(:bad_nid)        { "urn:example.com:foo" }
+  let(:percent_nid)    { "urn:example%41com:foo" }
+  let(:too_long_nid)   { "urn:example-example-example-example-x:foo" }
+  let(:empty_nss)      { "urn:example:" }
+  let(:naked_percent)  { "urn:example:%" }
+  let(:short_percent)  { "urn:example:%a" }
+  let(:nonhex_percent) { "urn:example:%ax" }
+
+  context "when testing a valid URN" do
+    it "should match a simple URN" do
+      expect(grok_match(pattern, simple)).to pass
+    end
+
+    it "should match a complex URN" do
+      expect(grok_match(pattern, unreserved)).to pass
+    end
+
+    it "should allow reserved characters" do
+      expect(grok_match(pattern, reserved)).to pass
+    end
+
+    it "should allow percent-escapes" do
+      expect(grok_match(pattern, escaped_upper)).to pass
+      expect(grok_match(pattern, escaped_lower)).to pass
+      expect(grok_match(pattern, only_escaped)).to pass
+    end
+
+    it "should match a URN with a 32-character NID" do
+      expect(grok_match(pattern, long_nid)).to pass
+    end
+  end
+
+  context "when testing an invalid URN" do
+    it "should reject capitalized 'URN'" do
+      expect(grok_match(pattern, bad_prefix)).not_to pass
+    end
+
+    it "should reject an empty NID" do
+      expect(grok_match(pattern, empty_nid)).not_to pass
+    end
+
+    it "should reject an NID with a leading hyphen" do
+      expect(grok_match(pattern, leading_hyphen)).not_to pass
+    end
+
+    it "should reject an NID with a special character" do
+      expect(grok_match(pattern, bad_nid)).not_to pass
+    end
+
+    it "should reject an NID with a percent sign" do
+      expect(grok_match(pattern, percent_nid)).not_to pass
+    end
+
+    it "should reject an NID longer than 32 characters" do
+      expect(grok_match(pattern, too_long_nid)).not_to pass
+    end
+
+    it "should reject a URN with an empty NSS" do
+      expect(grok_match(pattern, empty_nss)).not_to pass
+    end
+
+    it "should reject non-escape percent signs" do
+      expect(grok_match(pattern, naked_percent)).not_to pass
+      expect(grok_match(pattern, short_percent)).not_to pass
+      expect(grok_match(pattern, nonhex_percent)).not_to pass
+    end
+  end
 end
