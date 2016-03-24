27c6ecc165fcbbc8cb801db130801f8f1c7da55a
Colin Surprenant <colin.surprenant@gmail.com>
2016-03-24 14:05:04 -0400

    fix LOGSTASH_HOME path, add tmp pattern_path method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,20 @@ require 'rspec/expectations'
 # LOGSTASH_HOME will not be defined, so let's set it here
 # before requiring the grok filter
 unless LogStash::Environment.const_defined?(:LOGSTASH_HOME)
-  LogStash::Environment::LOGSTASH_HOME = File.expand_path("../", __FILE__)
+  LogStash::Environment::LOGSTASH_HOME = File.expand_path("../../", __FILE__)
+end
+
+# temporary fix to have the spec pass for an urgen mass-publish requirement.
+# cut & pasted from the same tmp fix in the grok spec
+# see https://github.com/logstash-plugins/logstash-filter-grok/issues/72
+# this needs to be refactored and properly fixed
+module LogStash::Environment
+  # also :pattern_path method must exist so we define it too
+  unless self.method_defined?(:pattern_path)
+    def pattern_path(path)
+      ::File.join(LOGSTASH_HOME, "patterns", path)
+    end
+  end
 end
 
 require "logstash/filters/grok"
