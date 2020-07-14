3b0ea51b93371b52cddfad4884ff2b2b4fec5378
Karol Bucek <self@kares.org>
2020-07-14 10:21:07 +0200

    Refactor: move patterns under (legacy) sub-dir
    part-of #297

--- lib/logstash/patterns/core.rb
+++ lib/logstash/patterns/core.rb
@@ -1,10 +1,10 @@
 module LogStash
   module Patterns
     module Core
-    extend self
+      extend self
 
       def path
-        ::File.expand_path('../../../patterns', ::File.dirname(__FILE__))
+        ::File.expand_path('../../../patterns/legacy', ::File.dirname(__FILE__))
       end
 
     end
