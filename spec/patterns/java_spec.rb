a57a1de837443c0d2d1b5d9c714a5b8f59179e7e
Karol Bucek <self@kares.org>
2020-07-21 18:02:27 +0200

    Test: add a specs for JAVASTACKTRACEPART

--- spec/patterns/java_spec.rb
+++ spec/patterns/java_spec.rb
@@ -16,3 +16,31 @@ describe "JAVA" do
     end
   end
 end
+
+describe "JAVASTACKTRACEPART" do
+  let(:pattern) { 'JAVASTACKTRACEPART' }
+  let(:message) { '  at com.sample.stacktrace.StackTraceExample.aMethod(StackTraceExample.java:42)' }
+  it "matches" do
+    grok = grok_match(pattern, message, true)
+    expect(grok).to include({
+                                "message"=>"  at com.sample.stacktrace.StackTraceExample.aMethod(StackTraceExample.java:42)",
+                                "method"=>"aMethod",
+                                "class"=>"com.sample.stacktrace.StackTraceExample",
+                                "file"=>"StackTraceExample.java",
+                                "line"=>"42"
+                            })
+  end
+
+  context 'generated file' do
+    let(:message) { '  at org.jruby.RubyMethod$INVOKER$i$call.call(RubyMethod$INVOKER$i$call.gen)' }
+    it "matches" do
+      grok = grok_match(pattern, message, true)
+      # TODO does not match
+      # expect(grok).to include({
+      #                             "method"=>"call",
+      #                             "class"=>"org.jruby.RubyMethod$INVOKER$i$call",
+      #                             "file"=>"RubyMethod$INVOKER$i$call.gen",
+      #                         })
+    end
+  end
+end
