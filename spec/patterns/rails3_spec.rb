73ae9ace344805b2b0c2e2423037c140a582ee1f
Karol Bucek <self@kares.org>
2020-07-30 20:55:04 +0200

    Test: RAILS3 pattern behavior in ECS mode

--- spec/patterns/rails3_spec.rb
+++ spec/patterns/rails3_spec.rb
@@ -2,17 +2,35 @@
 require "spec_helper"
 require "logstash/patterns/core"
 
-describe_pattern "RAILS3" do
+describe_pattern "RAILS3", ['legacy', 'ecs-v1'] do
 
   context "single-line log" do
 
     let(:message) { 'Started POST "/api/v3/internal/allowed" for 127.0.0.1 at 2015-08-05 11:37:01 +0200' }
 
     # Started
-    it { should include("verb" => "POST" ) }
-    it { should include("request" => "/api/v3/internal/allowed" ) }
+    it do
+      if ecs_compatibility?
+        should include("http" => hash_including("request" => { "method" => "POST" }))
+      else
+        should include("verb" => "POST")
+      end
+    end
+
+    it do
+      if ecs_compatibility?
+      else
+        should include("request" => "/api/v3/internal/allowed")
+      end
+    end
     # for
-    it { should include("clientip" => "127.0.0.1" ) }
+    it do
+      if ecs_compatibility?
+        should include("source" => { "address" => "127.0.0.1" })
+      else
+        should include("clientip" => "127.0.0.1")
+      end
+    end
     # at
     it { should include("timestamp" => "2015-08-05 11:37:01 +0200" ) }
   end
@@ -25,28 +43,72 @@ Processing by Projects::NotesController#index as JSON
 Completed 200 OK in 640ms (Views: 1.7ms | ActiveRecord: 91.0ms)' }
 
     # started
-    it { should include("verb" => "GET" ) }
-    it { should include("request" => "/puppet/postfix/notes?target_id=162&target_type=issue&last_fetched_at=1438695732" ) }
+    it do
+      if ecs_compatibility?
+        should include("http" => hash_including("request" => { "method" => "GET" }))
+      else
+        should include("verb" => "GET")
+      end
+    end
+
+    it do
+      if ecs_compatibility?
+        should include("url" => {"original"=>"/puppet/postfix/notes?target_id=162&target_type=issue&last_fetched_at=1438695732"})
+      else
+        should include("request" => "/puppet/postfix/notes?target_id=162&target_type=issue&last_fetched_at=1438695732" )
+      end
+    end
     # for
-    it { should include("clientip" => "127.0.0.1" ) }
+    it do
+      if ecs_compatibility?
+        should include("source" => { "address" => "127.0.0.1" })
+      else
+        should include("clientip" => "127.0.0.1")
+      end
+    end
     # at
-    it { should include("timestamp" => "2015-08-05 07:40:22 +0200" ) }
+    it { should include("timestamp" => "2015-08-05 07:40:22 +0200") }
     # Processing by
-    it { should include("controller" => "Projects::NotesController" ) }
-    it { should include("action" => "index" ) }
+    it do
+      if ecs_compatibility?
+        should include("rails" => hash_including("controller" => { "class"=>"Projects::NotesController", "action"=>"index" }))
+      else
+        should include("controller" => "Projects::NotesController")
+        should include("action" => "index")
+      end
+    end
     # as
-    it { should include("format" => "JSON" ) }
+    it do
+      if ecs_compatibility?
+        should include("rails" => hash_including("request" => hash_including("format" => 'JSON')))
+      else
+        should include("format" => "JSON" )
+      end
+    end
     # Parameters
-    it { should include("params" => '"target_id"=>"162", "target_type"=>"issue", "last_fetched_at"=>"1438695732", "namespace_id"=>"puppet", "project_id"=>"postfix"' ) }
+    it do
+      params = '"target_id"=>"162", "target_type"=>"issue", "last_fetched_at"=>"1438695732", "namespace_id"=>"puppet", "project_id"=>"postfix"'
+      if ecs_compatibility?
+        should include("rails" => hash_including("request" => hash_including("params" => params)))
+      else
+        should include("params" => params)
+      end
+    end
     # Completed
-    it { should include("response" => "200" ) }
+    it do
+      if ecs_compatibility?
+        should include("http" => hash_including("response" => { "status_code" => 200 }))
+      else
+        should include("response" => "200" )
+      end
+    end
     # in
-    it { should include("totalms" =>  "640" ) }
-    # (Views: 
-    it { should include("viewms" =>  "1.7" ) }
-    # | ActiveRecord:
-    it { should include("activerecordms" =>  "91.0" ) }
-
+    it do
+      if ecs_compatibility?
+        should include("rails" => hash_including("request" => hash_including("duration" => { "total" => 640.0, "view" => 1.7, "active_record" => 91.0 })))
+      else
+        should include("totalms" => "640", "viewms" => "1.7", "activerecordms" => "91.0")
+      end
+    end
   end
-
 end
