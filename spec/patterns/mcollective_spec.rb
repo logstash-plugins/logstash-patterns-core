4d27427605103d5564bcd71c30b305df39082364
Karol Bucek <self@kares.org>
2020-08-04 16:17:06 +0200

    Test: spec ECS compliant behavior

--- spec/patterns/mcollective_spec.rb
+++ spec/patterns/mcollective_spec.rb
@@ -2,21 +2,34 @@
 require "spec_helper"
 require "logstash/patterns/core"
 
-describe "MCOLLECTIVE" do
+describe_pattern "MCOLLECTIVE" do
 
-  let(:pattern) { "MCOLLECTIVE" }
-  let(:value) { "I, [2010-12-29T11:15:32.321744 #11479]  INFO -- : mcollectived:33 The Marionette Collective 1.1.0 started logging at info level" }
+  let(:message) { "I, [2010-12-29T11:15:32.321744 #11479]  INFO -- : mcollectived:33 The Marionette Collective 1.1.0 started logging at info level" }
 
-  subject { grok_match(pattern, value) }
+  it do
+    should include("timestamp" => "2010-12-29T11:15:32.321744")
+  end
 
-  it { should include("timestamp"=>"2010-12-29T11:15:32.321744") }
-  it { should include("pid"=>"11479") }
-  it { should include("event_level"=>"INFO") }
+  it do
+    if ecs_compatibility?
+      should include("process" => { "pid" => 11479 })
+    else
+      should include("pid" => "11479")
+    end
+  end
+
+  it do
+    if ecs_compatibility?
+      should include("log" => hash_including("level" => "INFO"))
+    else
+      should include("event_level" => "INFO")
+    end
+  end
 
   # NOTE: pattern seems unfinished - missing match of remaining message
   it 'should have extracted message' do
     # but did not :
-    expect( subject['message'] ).to eql value
+    expect( subject['message'] ).to eql message
   end
 
 end
