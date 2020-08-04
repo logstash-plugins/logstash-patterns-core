3236f511e896f7f55b2d3484aed8525d72b7d30b
Karol Bucek <self@kares.org>
2020-08-04 13:26:27 +0200

    Change: avoid referrer/user-agent quotes in apache logs

--- spec/patterns/httpd_spec.rb
+++ spec/patterns/httpd_spec.rb
@@ -19,8 +19,8 @@ describe "HTTPD_COMBINEDLOG" do
         'httpversion' => '1.1',
         'response' => '200',
         'bytes' => '203023',
-        'referrer' => '"http://semicomplete.com/presentations/logstash-monitorama-2013/"',
-        'agent' => '"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.77 Safari/537.36"'
+        'referrer' => 'http://semicomplete.com/presentations/logstash-monitorama-2013/',
+        'agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.77 Safari/537.36'
       )
     end
 
