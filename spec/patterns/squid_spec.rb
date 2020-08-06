cb43452b61f8ed384546c46ad09e32ceb4f7906b
Karol Bucek <self@kares.org>
2020-08-06 12:40:07 +0200

    Test: SQUID3 ecs behavior

--- spec/patterns/squid_spec.rb
+++ spec/patterns/squid_spec.rb
@@ -2,60 +2,84 @@
 require "spec_helper"
 require "logstash/patterns/core"
 
-describe "SQUID3" do
-
-  let(:grok) { grok_match("SQUID3", value) }
+describe_pattern "SQUID3", ['legacy', 'ecs-v1'] do
 
   describe 'CONNECT sample' do
 
-    let(:value) do
+    let(:message) do
       '1525344856.899  16867 10.170.72.111 TCP_TUNNEL/200 6256 CONNECT logs.ap-southeast-2.amazonaws.com:443 - HIER_DIRECT/53.140.206.134 -'
     end
 
     it "matches" do
-      expect(grok).to include(
-                          "timestamp" => "1525344856.899",
-                          "duration" => "16867",
-                          "client_address" => "10.170.72.111",
-                          "cache_result" => "TCP_TUNNEL",
-                          "status_code" => "200",
-                          "request_method" => "CONNECT",
-                          "bytes" => "6256",
-                          "url" => "logs.ap-southeast-2.amazonaws.com:443",
-                          "user" => "-",
-                          "hierarchy_code" => "HIER_DIRECT",
-                          "server" => "53.140.206.134",
-                          "content_type" => "-",
-                      )
+      expect(grok).to include("timestamp" => "1525344856.899")
+      if ecs_compatibility?
+        expect(grok).to include(
+                            "squid" => {
+                                "cache_result_code" => "TCP_TUNNEL",
+                                "hierarchy_code" => "HIER_DIRECT",
+                                "request" => { "duration" => 16867, "content_type" => "-" }
+                            })
+        expect(grok).to include("destination" => { "address" => "53.140.206.134" })
+        expect(grok).to include("http" => { "request" => { "method" => "CONNECT" }, "response" => { "bytes" => 6256, "status_code" => 200 } })
+        expect(grok).to include("url" => { "original" => "logs.ap-southeast-2.amazonaws.com:443" })
+        expect(grok).to include("source" => { "ip" => "10.170.72.111" })
+      else
+        expect(grok).to include(
+                            "duration" => "16867",
+                            "client_address" => "10.170.72.111",
+                            "cache_result" => "TCP_TUNNEL",
+                            "status_code" => "200",
+                            "request_method" => "CONNECT",
+                            "bytes" => "6256",
+                            "url" => "logs.ap-southeast-2.amazonaws.com:443",
+                            "user" => "-",
+                            "hierarchy_code" => "HIER_DIRECT",
+                            "server" => "53.140.206.134",
+                            "content_type" => "-",
+                            )
+      end
     end
 
   end
 
   describe 'GET sample' do
 
-    let(:value) do
+    let(:message) do
       "1525334330.556      3 120.65.1.1 TCP_REFRESH_MISS/200 2014 GET http://www.sample.com/hellow_world.txt public-user DIRECT/www.sample.com text/plain 902351708.872"
     end
 
     it "matches" do
-      expect(grok).to include(
-                          "timestamp"=>"1525334330.556",
-                          "duration"=>"3",
-                          "client_address"=>"120.65.1.1",
-                          "cache_result"=>"TCP_REFRESH_MISS",
-                          "status_code"=>"200",
-                          "bytes"=>"2014",
-                          "request_method" => "GET",
-                          "url" => "http://www.sample.com/hellow_world.txt",
-                          "user"=>"public-user",
-                          "hierarchy_code"=>"DIRECT",
-                          "server"=>"www.sample.com",
-                          "content_type"=>"text/plain",
-                      )
+      expect(grok).to include("timestamp" => "1525334330.556")
+      if ecs_compatibility?
+        expect(grok).to include(
+                            "squid" => {
+                                "cache_result_code" => "TCP_REFRESH_MISS",
+                                "request" => { "content_type" => "text/plain", "duration" => 3 }, "hierarchy_code" => "DIRECT"
+                            })
+        expect(grok).to include("destination" => { "address" => "www.sample.com" })
+        expect(grok).to include("http" => { "request" => { "method" => "GET" }, "response" => { "bytes" => 2014, "status_code" => 200 } })
+        expect(grok).to include("url" => { "original" => "http://www.sample.com/hellow_world.txt" })
+        expect(grok).to include("source" => { "ip" => "120.65.1.1" })
+        expect(grok).to include("user" => { "name" => "public-user" })
+      else
+        expect(grok).to include(
+                            "duration"=>"3",
+                            "client_address"=>"120.65.1.1",
+                            "cache_result"=>"TCP_REFRESH_MISS",
+                            "status_code"=>"200",
+                            "bytes"=>"2014",
+                            "request_method" => "GET",
+                            "url" => "http://www.sample.com/hellow_world.txt",
+                            "user"=>"public-user",
+                            "hierarchy_code"=>"DIRECT",
+                            "server"=>"www.sample.com",
+                            "content_type"=>"text/plain",
+                            )
+      end
     end
 
     it "retains message" do
-      expect(grok).to include("message" => value)
+      expect(grok).to include("message" => message)
     end
 
   end
