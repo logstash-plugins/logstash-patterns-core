fd7057ae6c548a0261695eb19c2ca1d0d00f1b64
Karol Bucek <self@kares.org>
2020-10-09 15:41:40 +0200

    adapt to new zeek (http.log) format

--- spec/patterns/bro_spec.rb
+++ spec/patterns/bro_spec.rb
@@ -157,3 +157,60 @@ describe_pattern "BRO_HTTP", ['legacy', 'ecs-v1'] do
   end
 
 end
+
+describe_pattern "ZEEK_HTTP", ['ecs-v1'] do
+
+  context "long message" do
+
+    let(:message) do
+      "1333458850.375568	ClEkJM2Vm5giqnMf4h	10.131.47.185	1923	79.101.110.141	80	1	GET	o-o.preferred.telekomrs-beg1.v2.lscache8.c.youtube.com	/videoplayback?upn=MTU2MDY5NzQ5OTM0NTI3NDY4NDc&sparams=algorithm,burst,cp,factor,id,ip,ipbits,itag,source,upn,expire&fexp=912300,907210&algorithm=throttle-factor&itag=34&ip=212.0.0.0&burst=40&sver=3&signature=832FB1042E20780CFCA77A4DB5EA64AC593E8627.D1166C7E8365732E52DAFD68076DAE0146E0AE01&source=youtube&expire=1333484980&key=yt1&ipbits=8&factor=1.25&cp=U0hSSFRTUl9NSkNOMl9MTVZKOjh5eEN2SG8tZF84&id=ebf1e932d4bd1286&cm2=1	http://s.ytimg.com/yt/swfbin/watch_as3-vflqrJwOA.swf	1.1	Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko; X-SBLSP) Chrome/17.0.963.83 Safari/535.11	-	0	56320	206	Partial Content	-	-	(empty)	-	-	-	-	-	-	FpmJd62pFQcZ3gcUgl	-	-"
+    end
+
+    it "matches" do
+      expect(grok).to include("timestamp"=>"1333458850.375568")
+      expect(grok).to include("zeek" => hash_including("session_id"=>"ClEkJM2Vm5giqnMf4h"))
+      expect(grok).to include("http" => {
+          "response" => { "body" =>{ "bytes" => 56320 }, "status_code"=>206 },
+          "request" => { "body" => { "bytes" => 0 }, "referrer"=>"http://s.ytimg.com/yt/swfbin/watch_as3-vflqrJwOA.swf", "method"=>"GET"},
+          "version"=>"1.1"
+      })
+      expect(grok).to include("user_agent"=>{"original"=>"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko; X-SBLSP) Chrome/17.0.963.83 Safari/535.11"})
+      expect(grok).to include("url"=>{
+          "domain"=>"o-o.preferred.telekomrs-beg1.v2.lscache8.c.youtube.com",
+          "original"=>"/videoplayback?upn=MTU2MDY5NzQ5OTM0NTI3NDY4NDc&sparams=algorithm,burst,cp,factor,id,ip,ipbits,itag,source,upn,expire&fexp=912300,907210&algorithm=throttle-factor&itag=34&ip=212.0.0.0&burst=40&sver=3&signature=832FB1042E20780CFCA77A4DB5EA64AC593E8627.D1166C7E8365732E52DAFD68076DAE0146E0AE01&source=youtube&expire=1333484980&key=yt1&ipbits=8&factor=1.25&cp=U0hSSFRTUl9NSkNOMl9MTVZKOjh5eEN2SG8tZF84&id=ebf1e932d4bd1286&cm2=1"
+      })
+      expect(grok).to include("destination"=>{"port"=>80, "ip"=>"79.101.110.141"}, "source"=>{"port"=>1923, "ip"=>"10.131.47.185"})
+      expect(grok).to include("zeek" => hash_including("http"=>{"resp_fuids"=>"FpmJd62pFQcZ3gcUgl", "status_msg"=>"Partial Content", "trans_depth"=>1}))
+    end
+
+  end
+
+  context "sample message" do
+
+    let(:message) do
+      "1602165002.455618	CWGOypTfRypTC5C4g	192.168.122.59	44136	216.58.201.110	80	1	-	-	-	-	1.1	Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0	https://example.com	0	219	301	Moved Permanently	-	-	FOO,BAR	-	-	-	-	-	-	FeJ7iiVorMXoLlRK	-	text/html"
+    end
+
+    it "matches" do
+      expect(grok).to include("timestamp"=>"1602165002.455618")
+      expect(grok).to include("user_agent"=>{"original"=>"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0"})
+      expect(grok).to include("http" => {
+          "request" => { "body" => {"bytes" => 0 } },
+          "response" => { "status_code" => 301, "body" =>{ "bytes" => 219 } },
+          "version"=>"1.1"
+      })
+      expect(grok).to include("zeek" => {
+          "session_id" => "CWGOypTfRypTC5C4g",
+          "http" => {
+              "trans_depth"=>1,
+              "origin"=>"https://example.com",
+              "tags"=>"FOO,BAR",
+              "resp_mime_types"=>"text/html",
+              "status_msg"=>"Moved Permanently",
+              "resp_fuids"=>"FeJ7iiVorMXoLlRK"}
+      })
+    end
+
+  end
+
+end
