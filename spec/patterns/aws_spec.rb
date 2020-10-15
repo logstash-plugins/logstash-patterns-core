0a551cc2aeb5d18e66b352ad877e21c928ba50ba
Karol Bucek <self@kares.org>
2020-10-15 19:04:31 +0200

    Test: mark specs that only pass in legacy mode

--- spec/patterns/aws_spec.rb
+++ spec/patterns/aws_spec.rb
@@ -2,7 +2,7 @@
 require "spec_helper"
 require "logstash/patterns/core"
 
-describe_pattern "ELB_ACCESS_LOG", ['legacy'] do
+describe_pattern "ELB_ACCESS_LOG", ['legacy', 'ecs-v1'] do
 
   context "parsing an access log" do
 
@@ -63,28 +63,28 @@ describe_pattern "S3_ACCESS_LOG", ['legacy'] do
       "79a5 mybucket [06/Feb/2014:00:00:38 +0000] 192.0.2.3 79a5 3E57427F3EXAMPLE REST.GET.VERSIONING - \"GET /mybucket?versioning HTTP/1.1\" 200 - 113 - 7 - \"-\" \"S3Console/0.4\" -"
     end
 
-    it { should include("owner" => "79a5" ) }
-    it { should include("bucket" => "mybucket" ) }
-    it { should include("timestamp" => "06/Feb/2014:00:00:38 +0000" ) }
-    it { should include("clientip" => "192.0.2.3" ) }
-    it { should include("requester" => "79a5" ) }
-    it { should include("request_id" => "3E57427F3EXAMPLE" ) }
-    it { should include("operation" => "REST.GET.VERSIONING" ) }
-    it { should include("key" => "-" ) }
+    it { should include("owner" => "79a5" ) unless ecs_compatibility? }
+    it { should include("bucket" => "mybucket" ) unless ecs_compatibility? }
+    it { should include("timestamp" => "06/Feb/2014:00:00:38 +0000" ) unless ecs_compatibility? }
+    it { should include("clientip" => "192.0.2.3" ) unless ecs_compatibility? }
+    it { should include("requester" => "79a5" ) unless ecs_compatibility? }
+    it { should include("request_id" => "3E57427F3EXAMPLE" ) unless ecs_compatibility? }
+    it { should include("operation" => "REST.GET.VERSIONING" ) unless ecs_compatibility? }
+    it { should include("key" => "-" ) unless ecs_compatibility? }
 
-    it { should include("verb" => "GET" ) }
-    it { should include("request" => "/mybucket?versioning" ) }
-    it { should include("httpversion" => "1.1" ) }
-    it { should include("response" => 200 ) }
-    it { should include("bytes" => 113 ) }
+    it { should include("verb" => "GET" ) unless ecs_compatibility? }
+    it { should include("request" => "/mybucket?versioning" ) unless ecs_compatibility? }
+    it { should include("httpversion" => "1.1" ) unless ecs_compatibility? }
+    it { should include("response" => 200 ) unless ecs_compatibility? }
+    it { should include("bytes" => 113 ) unless ecs_compatibility? }
 
-    it { should include("request_time_ms" => 7 ) }
-    it { should include("referrer" => "\"-\"" ) }
-    it { should include("agent" => "\"S3Console/0.4\"" ) }
+    it { should include("request_time_ms" => 7 ) unless ecs_compatibility? }
+    it { should include("referrer" => "\"-\"" ) unless ecs_compatibility? }
+    it { should include("agent" => "\"S3Console/0.4\"" ) unless ecs_compatibility? }
 
     ["tags", "error_code", "object_size", "turnaround_time_ms", "version_id"].each do |attribute|
       it "have #{attribute} as nil" do
-        expect(subject[attribute]).to be_nil
+        expect(subject[attribute]).to be_nil unless ecs_compatibility?
       end
     end
 
@@ -96,28 +96,28 @@ describe_pattern "S3_ACCESS_LOG", ['legacy'] do
       "79a5 mybucket [12/May/2014:07:54:01 +0000] 10.0.1.2 - 7ACC4BE89EXAMPLE REST.GET.OBJECT foo/bar.html \"GET /foo/bar.html HTTP/1.1\" 304 - - 1718 10 - \"-\" \"Mozilla/5.0\" -"
     end
 
-    it { should include("owner" => "79a5" ) }
-    it { should include("bucket" => "mybucket" ) }
-    it { should include("timestamp" => "12/May/2014:07:54:01 +0000" ) }
-    it { should include("clientip" => "10.0.1.2" ) }
-    it { should include("requester" => "-" ) }
-    it { should include("request_id" => "7ACC4BE89EXAMPLE" ) }
-    it { should include("operation" => "REST.GET.OBJECT" ) }
-    it { should include("key" => "foo/bar.html" ) }
+    it { should include("owner" => "79a5" ) unless ecs_compatibility? }
+    it { should include("bucket" => "mybucket" ) unless ecs_compatibility? }
+    it { should include("timestamp" => "12/May/2014:07:54:01 +0000" ) unless ecs_compatibility? }
+    it { should include("clientip" => "10.0.1.2" ) unless ecs_compatibility? }
+    it { should include("requester" => "-" ) unless ecs_compatibility? }
+    it { should include("request_id" => "7ACC4BE89EXAMPLE" ) unless ecs_compatibility? }
+    it { should include("operation" => "REST.GET.OBJECT" ) unless ecs_compatibility? }
+    it { should include("key" => "foo/bar.html" ) unless ecs_compatibility? }
 
-    it { should include("verb" => "GET" ) }
-    it { should include("request" => "/foo/bar.html" ) }
-    it { should include("httpversion" => "1.1" ) }
-    it { should include("response" => 304 ) }
-    it { should include("object_size" => 1718 ) }
+    it { should include("verb" => "GET" ) unless ecs_compatibility? }
+    it { should include("request" => "/foo/bar.html" ) unless ecs_compatibility? }
+    it { should include("httpversion" => "1.1" ) unless ecs_compatibility? }
+    it { should include("response" => 304 ) unless ecs_compatibility? }
+    it { should include("object_size" => 1718 ) unless ecs_compatibility? }
 
-    it { should include("request_time_ms" => 10 ) }
-    it { should include("referrer" => "\"-\"" ) }
-    it { should include("agent" => "\"Mozilla/5.0\"" ) }
+    it { should include("request_time_ms" => 10 ) unless ecs_compatibility? }
+    it { should include("referrer" => "\"-\"" ) unless ecs_compatibility? }
+    it { should include("agent" => "\"Mozilla/5.0\"" ) unless ecs_compatibility? }
 
     ["tags", "error_code", "turnaround_time_ms", "version_id", "bytes"].each do |attribute|
       it "have #{attribute} as nil" do
-        expect(subject[attribute]).to be_nil
+        expect(subject[attribute]).to be_nil unless ecs_compatibility?
       end
     end
 
