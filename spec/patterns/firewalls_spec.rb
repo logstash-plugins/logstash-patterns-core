0750c11acdcbd921005ef1076628391dd6d35870
Karol Bucek <self@kares.org>
2020-11-05 20:06:22 +0100

    ECS-ize SFW2 pattern

--- spec/patterns/firewalls_spec.rb
+++ spec/patterns/firewalls_spec.rb
@@ -109,3 +109,114 @@ describe_pattern "CISCOFW106023" do
   end
 
 end
+
+
+describe_pattern 'SFW2', ['legacy', 'ecs-v1'] do
+
+  let(:message) do
+    "Mar 8 22:35:42 linux kernel: SuSE-FW-DROP-DEFAULT IN=ppp0 OUT= MAC= SRC=202.175.181.4 DST=64.238.136.187 LEN=48 TOS=0x00 PREC=0x00 TTL=113 ID=31151 DF PROTO=TCP SPT=3360 DPT=3127 WINDOW=65340 RES=0x00 SYN URGP=0 OPT (020405AC01010402)"
+    # ^^^ parse-failure
+    "Jan 15 11:21:13 IKCSWeb kernel: SFW2-IN-ACC-RELATED IN=eth1 OUT= MAC=00:19:bb:2e:85:42:00:17:c5:d8:2e:2c:08:00 SRC=59.64.166.81 DST=207.194.99.122 LEN=40 TOS=0x00 PREC=0x00 TTL=64 ID=48949 DF PROTO=TCP SPT=24093 DPT=22 WINDOW=137 RES=0x00 ACK URGP=0"
+
+    "Jan 29 00:00:28 myth kernel: SFW2-INext-DROP-DEFLT IN=ppp0 OUT= MAC= SRC=24.64.208.134 DST=216.58.112.55 LEN=512 TOS=0x00 PREC=0x00 TTL=70 ID=55012 PROTO=UDP SPT=24128 DPT=1026 LEN=492"
+  end
+
+  it 'matches' do
+    if ecs_compatibility?
+      expect(grok).to include(
+                          "timestamp"=>"Jan 29 00:00:28",
+                          "host"=>{"hostname"=>"myth"},
+                           "destination"=>{"ip"=>"216.58.112.55", "port"=>1026}, "source"=>{"ip"=>"24.64.208.134", "port"=>24128},
+                           "suse"=>{"firewall"=>{"action"=>"DROP-DEFLT"}},
+                           "iptables"=>{
+                               "input_interface"=>"ppp0",
+                               "length"=>512,
+                               "id"=>"55012",
+                               "ttl"=>70,
+                               "tos"=>"00",
+                               "precedence_bits"=>"00"}, "suse"=>{"firewall"=>{"action"=>"DROP-DEFLT"}}, "host"=>{"hostname"=>"myth"
+                          },
+                          "network"=>{"transport"=>"UDP"}
+                      )
+    else
+      expect(grok).to include(
+                          "nf_action"=>"DROP-DEFLT",
+                          "nf_in_interface"=>"ppp0",
+                          "nf_src_ip"=>"24.64.208.134",
+                          "nf_dst_ip"=>"216.58.112.55",
+                          "nf_protocol"=>"UDP"
+                          )
+    end
+  end
+
+  context 'long message' do
+
+    let(:message) do
+      'Mar  8 20:16:44 black kernel: [20474.050964] SFW2-INext-ACC-TCP IN=eth0 OUT= MAC=28:45:a7:f3:18:00:00:22:15:67:6a:25:08:00 SRC=192.168.0.101 DST=192.168.0.100 LEN=60 TOS=0x00 PREC=0x00 TTL=64 ID=6429 DF PROTO=TCP SPT=59282 DPT=631 WINDOW=14600 RES=0x00 SYN URGP=0 OPT (020405B40402080A00825F5D0000000001030307)'
+    end
+
+    it 'matches' do
+      if ecs_compatibility?
+        expect(grok).to include(
+                            "timestamp"=>"Mar  8 20:16:44",
+                            "host"=>{"hostname"=>"black"},
+                            "suse"=>{"firewall"=>{"action"=>"ACC-TCP"}},
+                            "destination"=>{"mac"=>"28:45:a7:f3:18:00", "ip"=>"192.168.0.100", "port"=>631},
+                            "source"=>{"mac"=>"00:22:15:67:6a:25", "ip"=>"192.168.0.101", "port"=>59282},
+                            "iptables"=>{
+                                "input_interface"=>"eth0",
+                                "length"=>60,
+                                "id"=>"6429",
+                                "tos"=>"00", "fragment_flags"=>"DF",
+                                "ttl"=>64,
+                                "precedence_bits"=>"00",
+                                "tcp"=>{"flags"=>"SYN ", "window"=>14600},
+                                "tcp_reserved_bits"=>"00"
+                            },
+                            "network"=>{"transport"=>"TCP"}
+                        )
+      else
+        expect(grok).to include(
+                            "nagios_epoch"=>"20474.050964", # YAY!
+                            "nf_action"=>"ACC-TCP",
+                            "nf_in_interface"=>"eth0",
+                            "nf_dst_port"=>"631",
+
+                            "nf_dst_mac"=>"28:45:a7:f3:18:00",
+                            "nf_src_mac"=>"00:22:15:67:6a:25",
+                            "nf_dst_ip"=>"192.168.0.100",
+                            "nf_src_ip"=>"192.168.0.101",
+                            "nf_src_port"=>"59282",
+
+                            "nf_protocol"=>"TCP"
+                        )
+      end
+    end
+
+  end
+
+  context 'IPv6 message' do
+
+    let(:message) do
+      "Jan 15 11:21:13 IKCSWeb kernel: SFW2-INext-ACC-RELATED IN=eth0 OUT= MAC= SRC=fe80:0000:0000:0000:16da:e9ff:feec:a04d DST=ff02:0000:0000:0000:0000:0000:0000:00fb LEN=527 TC=0 HOPLIMIT=255 FLOWLBL=804001 PROTO=UDP SPT=5353 DPT=5353 LEN=487"
+    end
+
+    it 'matches' do
+      if ecs_compatibility?
+        expect(grok).to include("iptables"=>{"input_interface"=>"eth0", "ttl"=>255, "flow_label"=>"804001", "length"=>527})
+        expect(grok).to include(
+                            "source"=>{"ip"=>"fe80:0000:0000:0000:16da:e9ff:feec:a04d", "port"=>5353},
+                            "destination"=>{"ip"=>"ff02:0000:0000:0000:0000:0000:0000:00fb", "port"=>5353},
+                            "network"=>{"transport"=>"UDP"}
+                        )
+      else
+        expect(grok).to include("nf_src_ip"=>"fe80:0000:0000:0000:16da:e9ff:feec:a04d",
+                                "nf_dst_ip"=>"ff02:0000:0000:0000:0000:0000:0000:00fb",
+                                "nf_protocol"=>"UDP")
+      end
+    end
+
+  end
+
+end
+
